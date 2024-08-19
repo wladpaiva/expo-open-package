6a7bec5bb50fab25301b8e8781b7a9f38f8488e1
Wlad Paiva <desenvolvedormg@gmail.com>
2024-08-19 13:46:29 -0300

    remove ios

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import { UnavailabilityError } from "expo-modules-core";
+
 // Import the native module. On web, it will be resolved to ExpoOpenPackage.web.ts
 // and on native platforms to ExpoOpenPackage.ts
-import ExpoOpenPackageModule from "./ExpoOpenPackageModule";
+import ExpoOpenPackageModule from "./ExpoOpenPackageLauncher";
 
 export async function openApplication(packageName: string): Promise<void> {
-  await ExpoOpenPackageModule.openApplication(packageName);
+  if (!ExpoOpenPackageModule.startActivity) {
+    throw new UnavailabilityError("IntentLauncher", "startActivityAsync");
+  }
+
+  if (!packageName || typeof packageName !== "string") {
+    throw new TypeError(`'packageName' argument must be a non-empty string!`);
+  }
+  return await ExpoOpenPackageModule.openApplication(packageName);
 }
